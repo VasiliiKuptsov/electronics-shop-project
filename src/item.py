3a3bc50ad9ab5c580cb876c3b73bd2e4c8b18d9d
Vasilii Kuptsov <kuptsovy@mail.ru>
2023-07-27 00:40:37 +0300

    homework only one

--- src/item.py
+++ src/item.py
@@ -6,25 +6,19 @@ class Item:
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        self.all.append(self)
 
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
 
     def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+        return self.price * self.quantity
 
-        :return: Общая стоимость товара.
-        """
-        pass
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.pay_rate
+        return self.price
