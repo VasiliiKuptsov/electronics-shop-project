2d63b508f5e797d16cf8b1eb03a49256f238fd31
Vasilii Kuptsov <kuptsovy@mail.ru>
2023-07-29 09:48:34 +0300

    homework3

--- src/item.py
+++ src/item.py
@@ -1,3 +1,11 @@
+
+import csv
+from pathlib import Path
+ROOT = Path(__file__).parent
+DATA_PATH = Path.joinpath(ROOT, 'items.csv')
+path = DATA_PATH
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -9,7 +17,6 @@ class Item:
         self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(self)
 
 
     def calculate_total_price(self) -> float:
@@ -21,4 +28,35 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
-        return self.price
+        return
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name_):
+        if len(name_) > 10:
+            self.__name = name_[0:10]
+        else:
+            self.__name = name_
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open(path, 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for reader_ in reader:
+                all_ = [reader_['name'], reader_['price'], reader_['quantity']]
+                cls.all.append(all_)
+        return
+
+    @staticmethod
+    def string_to_number(number: str) -> int:
+        return int(float(number))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
+
+    def __str__(self):
+        return (self.__name)
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -4,15 +4,9 @@ from src.item import Item
 
 def test_item():
     item_ = Item('pen', 300, 30)
+
+    assert item_.name == 'pen'
     assert item_.price == 300
     assert item_.quantity == 30
-    assert item_.calculate_total_price() == 9000
-    assert item_.apply_discount() == 300
-    Item.pay_rate = 0.8
-    assert item_.apply_discount() == 240
-    #assert item_.name == 'pen'
-    #item_.name = 'pencilbox-pen'
-    #assert item_.name == 'pencilbox-'
-    #Item.instantiate_from_csv()
-    #assert len(Item.all) == 5
-    #assert Item.string_to_number('5.0') == 5
+    assert repr(item_) == "Item('pen', 300, 30)"
+    assert str(item_) == 'pen'
